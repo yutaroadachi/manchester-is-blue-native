ba2e7022eb765bc3377c82627ac3952110f607eb
yutaroadachi <ytr_adc72@yahoo.co.jp>
2020-08-20 22:38:05 +0900

    feat: react-native-paperを導入

--- App.tsx
+++ App.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
+import { COLOR_PALETTE } from './src/constants/colorPalette';
 import { Routes } from './src/routes';
 import { StatusBar } from 'expo-status-bar';
 
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: COLOR_PALETTE.MAIN,
+    accent: COLOR_PALETTE.ACCENT,
+    background: COLOR_PALETTE.BASE,
+  },
+};
+
 export default function App() {
   return (
-    <>
+    <PaperProvider theme={theme}>
       <Routes />
       <StatusBar style="auto" />
-    </>
+    </PaperProvider>
   );
 }
--- src/components/screens/Home/index.tsx
+++ src/components/screens/Home/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { ParamList } from '../../../routes/HomeStack';
-import { View, Text, Button } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import { Card, Title, Paragraph } from 'react-native-paper';
 import { ROUTE_NAME } from '../../../constants/routeName';
 
 type Props = {
@@ -10,12 +11,20 @@ type Props = {
 
 export const Home = (props: Props) => {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>ホーム</Text>
-      <Button
-        title="試合詳細へ"
-        onPress={() => props.navigation.navigate(ROUTE_NAME.GAME_DETAIL)}
-      />
+    <View style={styles.container}>
+      <Card onPress={() => props.navigation.navigate(ROUTE_NAME.GAME_DETAIL)}>
+        <Card.Content>
+          <Title>ホーム</Title>
+          <Paragraph>hoge</Paragraph>
+        </Card.Content>
+      </Card>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+});
